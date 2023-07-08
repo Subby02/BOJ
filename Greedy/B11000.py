78a187088e6cb4db2a47e02f7138df13214880ff
Subby02 <95352085+Subby02@users.noreply.github.com>
2023-07-08 06:44:44 +0000

    Update 2023.07.08

---
+++ Greedy/B11000.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+
+array = []
+for _ in range(n):
+  s, t = map(int,input().split())
+  array.append((s,t))
+
+array.sort()
+
+room = []
+heapq.heappush(room,array[0][1])
+
+for i in range(1,n):
+  if array[i][0] < room[0]:
+    heapq.heappush(room,array[i][1])
+  else:
+    heapq.heappop(room)
+    heapq.heappush(room,array[i][1])
+
+print(len(room))
---
+++ Greedy/B21314.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+n = input().rstrip()
+
+max_str = ''
+min_str = ''
+cnt = 0
+for s in n:
+  if s == 'M':
+    cnt += 1
+  elif s == 'K':
+    if cnt == 0:
+      min_str += '5'
+    else:
+      min_str += str(10**(cnt - 1)) + '5'
+    max_str += str(10**cnt * 5)
+    cnt = 0
+
+if cnt != 0:
+  max_str += '1' * cnt
+  min_str += str(10**(cnt - 1))
+
+print(max_str)
+print(min_str)
